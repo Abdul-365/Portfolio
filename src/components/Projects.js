77290c24c0db26ecf8b75d847f112ac55e923a9f
Abdul Mannan <mannansaudi@gmail.com>
2023-07-18 10:29:56 +0530

    Made Projects Responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel';
 import Col from 'react-bootstrap/Col';
@@ -9,38 +9,35 @@ import projects from './projects.json';
 
 function ProjectCard({ project }) {
     return (
-        <Col className='d-flex justify-content-center'>
-            <Card>
-                <a className='h-100' href={project.link}>
-                    <Card.Img
-                        style={{
-                            objectFit: 'cover',
-                            maxHeight: '13.4em'
-                        }}
-                        variant="top"
-                        src={project.image}
-                    />
-                    <Card.Body className='d-flex flex-column p-4'>
-                        <Card.Title className='mb-4 fs-3'>{project.title}</Card.Title>
-                        <Card.Text className='d-flex flex-column mb-auto'>
-                            {project.description}
-                        </Card.Text>
-                        <div className='d-flex justify-content-between mt-3 flex-wrap gap-3'>
-                            {project.techStack.map((tech) => (
-                                <div className='d-flex align-items-center' key={tech.name}>
-                                    <img
-                                        style={{ width: '2rem', marginRight: '0.5em' }}
-                                        src={tech.logo}
-                                        alt={tech.name}
-                                    />
-                                    <p className='m-0 d-inline'>{tech.name}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </Card.Body>
-                </a>
-            </Card>
-        </Col>
+        <Card>
+            <a className='d-flex flex-column h-100' href={project.link}>
+                <Card.Img
+                    style={{
+                        objectFit: 'cover',
+                    }}
+                    variant="top"
+                    src={project.image}
+                />
+                <Card.Body className='d-flex flex-column p-4 flex-grow-1'>
+                    <Card.Title className='mb-4 fs-3'>{project.title}</Card.Title>
+                    <Card.Text style={{ whiteSpace: 'pre-wrap' }} className='mb-auto'>
+                        {project.description}
+                    </Card.Text>
+                    <div className='d-flex justify-content-between mt-4 flex-wrap gap-3'>
+                        {project.techStack.map((tech) => (
+                            <div className='d-flex align-items-center' key={tech.name}>
+                                <img
+                                    style={{ width: '2rem', marginRight: '0.5em' }}
+                                    src={tech.logo}
+                                    alt={tech.name}
+                                />
+                                <p className='m-0 d-inline'>{tech.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                </Card.Body>
+            </a>
+        </Card>
     )
 }
 
@@ -64,28 +61,33 @@ export default function Projects() {
                     </Col>
                 </Row>
                 <Row>
-                    <Carousel className='d-none d-xl-flex justify-content-center' interval={null}>
+                    <Carousel
+                        className='d-none d-xl-flex justify-content-center'
+                        interval={null}
+                    >
                         {slides.map((slide, index) => (
                             <Carousel.Item key={index}>
-                                <Container className='gap-1'>
-                                    <Row>
+                                <Container className='gap-1 h-100'>
+                                    <Row className='h-100'>
                                         {slide.map(project => (
-                                            <ProjectCard key={project.title} project={project} />
+                                            <Col key={project.title} className='d-flex justify-content-center'>
+                                                <ProjectCard project={project} />
+                                            </Col>
                                         ))}
                                     </Row>
                                 </Container>
                             </Carousel.Item>
                         ))}
                     </Carousel>
-                    {/* <Carousel className='d-flex d-xl-none justify-content-center' interval={null}>
+                    <Carousel className='d-block d-xl-none' interval={null}>
                         {projects.map(project => (
-                            <Carousel.Item key={project.title}>
+                            <Carousel.Item key={project.title} className='px-2'>
                                 <div className='d-flex justify-content-center'>
                                     <ProjectCard project={project} />
                                 </div>
                             </Carousel.Item>
                         ))}
-                    </Carousel> */}
+                    </Carousel>
                 </Row>
             </Col>
         </Row>
